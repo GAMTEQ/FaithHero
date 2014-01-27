f82ce7f7bccfa39bc6a60e65895806f21875429b
王 崇欢 <Christian@oumatoMacBook-Pro.local>
2014-01-27 23:47:28 +0800

    增加支持C++访问Lua

---
+++ ConfigLoader.cpp
@@ -0,0 +1,9 @@
+//
+//  ConfigLoader.cpp
+//  mmo2
+//
+//  Created by 王 崇欢 on 14-1-27.
+//
+//
+
+#include "ConfigLoader.h"
---
+++ ConfigLoader.h
@@ -0,0 +1,14 @@
+//
+//  ConfigLoader.h
+//  mmo2
+//
+//  Created by 王 崇欢 on 14-1-27.
+//
+//
+
+#ifndef mmo2_ConfigLoader_h
+#define mmo2_ConfigLoader_h
+
+
+
+#endif
