c151820e6595742c189dc942efe1fa46e98fc82b
王 崇欢 <Christian@oumatoMacBook-Pro.local>
2014-02-09 02:01:44 +0800

    登录界面布局

--- UserScene/LoginScene.cpp
+++ UserScene/LoginScene.cpp
@@ -19,54 +19,71 @@ bool LoginScene::init()
         CCPoint visibleOrigin = CCEGLView::sharedOpenGLView()->getVisibleOrigin();
         CCSize visibleSize = CCEGLView::sharedOpenGLView()->getVisibleSize();
         // 用户名
-        CCSize editBoxSize = CCSizeMake(visibleSize.width - 100, 60);
-        _editBoxUserName = CCEditBox::create(editBoxSize , CCScale9Sprite::create("green_edit.png"));
+        CCScale9Sprite* editbox9Sprite = CCScale9Sprite::create("editbox.png");
+        _editBoxUserName = CCEditBox::create(editbox9Sprite->boundingBox().size, editbox9Sprite, NULL, NULL);
         _editBoxUserName->setPosition(ccp(visibleOrigin.x + visibleSize.width / 2,
-                                          visibleOrigin.y + visibleSize.height * 3 / 4));
-        _editBoxUserName->setFont("Paint Boy", 25);
-        _editBoxUserName->setFontColor(ccBLACK);
-        _editBoxUserName->setPlaceHolder("Name:");
+                                          visibleOrigin.y + visibleSize.height * 0.6));
+        _editBoxUserName->setFont("American Typewriter", 50);
+        _editBoxUserName->setFontColor(ccWHITE);
+        _editBoxUserName->setPlaceHolder("用户名：");
         _editBoxUserName->setPlaceholderFontColor(ccWHITE);
         _editBoxUserName->setMaxLength(8);
         _editBoxUserName->setReturnType(kKeyboardReturnTypeDone);
         _editBoxUserName->setDelegate(this);
         this->addChild(_editBoxUserName, zLoginSceneEditBox);
         // 密码
-        _editBoxUserPassword = CCEditBox::create(editBoxSize, CCScale9Sprite::create("green_edit.png"));
-        _editBoxUserPassword->setPosition(ccp(visibleOrigin.x + visibleSize.width / 2,
-                                              visibleOrigin.y + visibleSize.height / 2));
-        _editBoxUserPassword->setFont("American Typewriter", 30);
-        _editBoxUserPassword->setFontColor(ccBLACK);
-        _editBoxUserPassword->setPlaceHolder("Password:");
+        CCScale9Sprite* editbox9Sprite1 = CCScale9Sprite::create("editbox.png");
+        _editBoxUserPassword = CCEditBox::create(editbox9Sprite1->boundingBox().size, editbox9Sprite1, NULL, NULL);
+        _editBoxUserPassword->setPosition(ccp(_editBoxUserName->getPositionX(),
+                                              _editBoxUserName->getPositionY() - editbox9Sprite1->boundingBox().size.height * 1.7));
+        _editBoxUserPassword->setFont("American Typewriter", 50);
+        _editBoxUserPassword->setFontColor(ccWHITE);
+        _editBoxUserPassword->setPlaceHolder("密码：");
         _editBoxUserPassword->setPlaceholderFontColor(ccWHITE);
         _editBoxUserPassword->setMaxLength(8);
         _editBoxUserPassword->setReturnType(kKeyboardReturnTypeDone);
         _editBoxUserPassword->setDelegate(this);
         this->addChild(_editBoxUserPassword, zLoginSceneEditBox);
         // 按钮
-        CCLabelTTF *label = CCLabelTTF::create("Login", "Arial", 50);
-        CCMenuItemLabel *pMenuItem = CCMenuItemLabel::create(label, this, menu_selector(LoginScene::onButtonLogin));
-        pMenuItem->setPosition(ccp(visibleOrigin.x + visibleSize.width / 2,
-                                   visibleOrigin.y + visibleSize.height / 4));
-        CCMenuItemImage *pCloseItem = CCMenuItemImage::create("close_normal.png",
-                                                              "close_selected.png",
-                                                              this,
-                                                              menu_selector(LoginScene::onButtonClose));
-        pCloseItem->setPosition(ccp(visibleOrigin.x + visibleSize.width - pCloseItem->getContentSize().width/2 ,
-                                    visibleOrigin.y + pCloseItem->getContentSize().height/2));
-        _loginMenu = CCMenu::create(pMenuItem, pCloseItem, NULL);
+        //CCLabelTTF *label = CCLabelTTF::create("Login", "Arial", 50);
+        //CCMenuItemLabel *pMenuItem = CCMenuItemLabel::create(label, this, menu_selector(LoginScene::onButtonLogin));
+        // 登录按钮
+        CCMenuItemImage* menuItemLogin = CCMenuItemImage::create("button_login.png",
+                                                                 "button_login_pressed.png",
+                                                                 this,
+                                                                 menu_selector(LoginScene::onButtonLogin));
+        menuItemLogin->setPosition(ccp(_editBoxUserPassword->getPositionX() - menuItemLogin->boundingBox().size.width,
+                                       _editBoxUserPassword->getPositionY() - _editBoxUserPassword->boundingBox().size.height * 1.7));
+        // 注册按钮
+        CCMenuItemImage *menuItemRegister = CCMenuItemImage::create("button_register.png",
+                                                                    "button_register_pressed.png",
+                                                                    this,
+                                                                    menu_selector(LoginScene::onButtonLogin));
+        menuItemRegister->setPosition(ccp(_editBoxUserPassword->getPositionX() + menuItemRegister->boundingBox().size.width,
+                                          _editBoxUserPassword->getPositionY() - _editBoxUserPassword->boundingBox().size.height * 1.7));
+        // 退出按钮
+        CCMenuItemImage *menuItemExit = CCMenuItemImage::create("close_normal.png",
+                                                                "close_selected.png",
+                                                                this,
+                                                                menu_selector(LoginScene::onButtonClose));
+
+        menuItemExit->setScale(2.0);
+        menuItemExit->setPosition(ccp(visibleOrigin.x + visibleSize.width - menuItemExit->getContentSize().width * menuItemExit->getScale() / 2,
+                                    visibleOrigin.y + menuItemExit->getContentSize().height * menuItemExit->getScale() / 2));
+        _loginMenu = CCMenu::create(menuItemLogin, menuItemRegister, menuItemExit, NULL);
         _loginMenu->setPosition(CCPointZero);
         this->addChild(_loginMenu, zLoginSceneEditBox);
         // LOGO
-        CCLabelTTF *pLabel = CCLabelTTF::create("Massive-Multiplayer-Online-2", "Arial", 30);
-        CC_BREAK_IF(!pLabel);
-        CCSize size = CCDirector::sharedDirector()->getWinSize();
-        pLabel->setPosition(ccp(size.width / 2, size.height - 30));
-        this->addChild(pLabel, zLoginSceneLogo);
+        //CCLabelTTF *pLabel = CCLabelTTF::create("Massive-Multiplayer-Online-2", "Arial", 30);
+        //CC_BREAK_IF(!pLabel);
+        //CCSize size = CCDirector::sharedDirector()->getWinSize();
+        //pLabel->setPosition(ccp(size.width / 2, size.height - 30));
+        //this->addChild(pLabel, zLoginSceneLogo);
         // 背景图片
         CCSprite *pSprite = CCSprite::create("login_scene_background.png");
         CC_BREAK_IF(!pSprite);
-        pSprite->setPosition(ccp(size.width / 2, size.height / 2));
+        pSprite->setPosition(ccp(visibleOrigin.x + visibleSize.width / 2,
+                                 visibleOrigin.y + visibleSize.height / 2));
         this->addChild(pSprite, zLoginSceneBackground);
         
         bRet = true;
@@ -110,6 +127,10 @@ void LoginScene::onButtonLogin(CCObject* pSender)
 {
     CCLog("LoginScene::onLogin");
     CCLog("UserName: %s UserPassword: %s", USER_INFO->getUserName().c_str(), USER_INFO->getUserPassword().c_str());
+    if(USER_INFO->getUserName() == "" || USER_INFO->getUserPassword() == "") {
+        CCLog("no name of pwd input");
+        return;
+    }
     //USER_INFO->setUserState(1);
     login();
     CCLog("login finish, wait for connector callback(client thread)");
